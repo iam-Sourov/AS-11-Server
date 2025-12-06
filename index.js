34ff912bf014cc95ca8281fd73e21fe484be0609
Sourov-Ahmed26 <silenboyk7@gmail.com>
2025-12-06 21:49:19 +0600

    post and get setup for book an user

--- index.js
+++ index.js
@@ -26,43 +26,48 @@ async function run() {
   try {
 
     await client.connect();
-
-
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
 
     const database = client.db("myDB");
-    const userCollection = database.collection("users"); 
-    const booksCollection = database.collection("books"); 
+    const userCollection = database.collection("users");
+    const booksCollection = database.collection("books");
 
-
+    
+    app.post('/users', async (req, res) => {
+      const newUser = req.body;
+      const query = { email: newUser.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'User already exists', insertedId: null });
+      }
+      const result = await userCollection.insertOne(newUser);
+      res.send(result);
+    });
     // GET: Read all users
     app.get('/users', async (req, res) => {
       const cursor = userCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
-    app.get('/books', async (req, res) => {
-        const cursor = booksCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
-    });
-
-    // POST:
     app.post('/users', async (req, res) => {
       const newUser = req.body;
       const result = await userCollection.insertOne(newUser);
       res.send(result);
     });
-    app.post('/books', async (req, res) => {
-        const newBook = req.body;
-        console.log("Adding new book:", newBook);
-        const result = await booksCollection.insertOne(newBook);
-        res.send(result);
+    //Books
+    app.get('/books', async (req, res) => {
+      const cursor = booksCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
     });
-
-
+    app.post('/books', async (req, res) => {
+      const newBook = req.body;
+      console.log("Adding new book:", newBook);
+      const result = await booksCollection.insertOne(newBook);
+      res.send(result);
+    });
     app.listen(port, () => {
       console.log(`🚀 Server is running on port: ${port}`);
     });
